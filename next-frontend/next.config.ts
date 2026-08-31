c4fb3bd1f30102e1d5772770bf310f864ba97e9d
TAGNE-JULIO <juliotagnetegueo@gmail.com>
2026-08-31 09:54:28 +0200

    Update API URL for production environment

--- next-frontend/next.config.ts
+++ next-frontend/next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output:'export',
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://orc-api.onrender.com",
   },
 };
 
