2830dc5bfb5f6050ed3d0329917b55fdd38ef670
TAGNE-JULIO <juliotagnetegueo@gmail.com>
2026-08-31 12:29:00 +0200

    Update next.config.ts

--- next-frontend/next.config.ts
+++ next-frontend/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output:'export',
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://ocr-studio.onrender.com",
-  },
 };
 
 export default nextConfig;
