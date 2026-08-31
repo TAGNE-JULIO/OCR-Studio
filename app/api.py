6844aca532a800271fde31859f91c80fd14735e7
TAGNE-JULIO <juliotagnetegueo@gmail.com>
2026-08-31 11:36:12 +0200

    Update api.py

--- app/api.py
+++ app/api.py
@@ -91,7 +91,7 @@ async def analyze_image(
         "matrice": matrice
     }
 
-# if __name__ == "__main__":
-#     # Point d'entrée pour lancer le serveur API complet
-#     print("Démarrage du serveur web sur http://localhost:8000")
-#     uvicorn.run("api:app", host="127.0.0.1", port=8000, reload=True)
+if __name__ == "__main__":
+    # Point d'entrée pour lancer le serveur API complet
+    print("Démarrage du serveur web sur http://localhost:8000")
+   uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
